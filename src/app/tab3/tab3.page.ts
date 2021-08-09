5f7b524a85fa1fdbd5e1a69172107e0a462aa982
Enrique Barroso <enrique21102001@hotmail.com>
2021-08-09 16:02:55 -0600

    Edit pagina cuando usuario logueado
    Se edito la pagina un usuario esta logueado

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -8,19 +8,27 @@ import { User} from './user.model';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page{
-  username: string = "";
+  username: string = " ";
   rol: string = " ";
+  phoneNumber: string = "";
   user: User[];
   constructor(public firebaseService: FirebaseServiceService) {}
 
   ngOnInit() {
-
+    if(this.username.length < 8 && this.rol !== ' '){
+      document.getElementById("profile-title-user").setAttribute("style" , "font-size: 50px;");
+      console.log("entro");
+    }
   }
   ionViewWillEnter(){
     if(this.firebaseService.userlogued[0] !== undefined){
       this.user =  this.firebaseService.userlogued;
       this.rol = this.user[0].rol;
+      this.phoneNumber = this.user[0].phoneNumber.toString().substring(0,4) + "-" + this.user[0].phoneNumber.toString().substring(4,8);
       this.username = this.user[0].fullName;
     }
+  }
+  logOut(){
+
   }
 }
